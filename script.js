907a4b6e50b6d258328e6ec3ca7717f6e5313ee6
uttsav1025 <74333675+uttsav1025@users.noreply.github.com>
2023-10-14 16:26:04 +0545

    corrected formula

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculateMaturityAmount(){
     const interestRate= parseFloat(document.getElementById('interestRate').value)
     const tenure= parseFloat(document.getElementById('tenure').value)
 
-    const maturityAmount= principal*(principal*interestRate*tenure*0.001)
+    const maturityAmount= principal+(principal*interestRate*tenure*0.001)
 
     document.getElementById('result').innerText=`Maturity Amount: ${maturityAmount.toFixed(2)}`;
 }
